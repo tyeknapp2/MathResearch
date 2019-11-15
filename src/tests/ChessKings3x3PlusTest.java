16012c5d3978a20515a25c1b7f3ebae01224a152
Tyler Knappenberger <tyeknapp.pro@gmail.com>
2019-11-15 12:19:39 -0500

    Fixed the All Tests function

--- src/tests/ChessKings3x3PlusTest.java
+++ src/tests/ChessKings3x3PlusTest.java
@@ -3,9 +3,9 @@
  */
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
